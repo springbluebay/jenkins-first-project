33482710550ff6d39ed1eee8fe7473522b3a5e52
EC2 Default User <ec2-user@ip-172-31-60-47.ec2.internal>
2020-08-16 14:16:49 +0000

    yyy

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 pipeline {
-            agent { docker { image 'python:alpine' } }
-                stages {
-                            stage('run') {
-                                            steps {
-                                                                echo 'Clarusway_Way to Reinvent Yourself'
-                                                                                sh 'python3 --version'
-                                                                                                sh 'python3 pipeline.py'
-                                                                                                            }
-                                                    }
-                                }
-                }
+    agent { docker { image 'python:alpine' } }
+    stages {
+        stage('run') {
+            steps {
+                echo 'Clarusway_Way to Reinvent Yourself'
+                sh 'python3 --version'
+                sh 'python3 pipeline.py'
+            }
+        }
+    }
+}
